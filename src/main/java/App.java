47379f1c83362b635926af0f5f888fa62e3dc60f
Charles Carter and Joshua Gustafson <silversevenfox@yahoo.com>
2016-02-08 13:35:48 -0800

    add initial files w/ first spec and code to pass

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class App {
+  public static void main(String[] args) {
+
+  }
+public static String leetspeak(String normalWord) {
+  String leetWord = normalWord.replaceAll("e", "3");
+  return leetWord;
+}
+
+}
